d602eedb9ffc513d48ac75fe7c19d81334bfbff4
raman <raman@10.0.0.236>
2018-06-12 22:59:04 -0400

    crud student repo

--- studentdal/src/main/java/com/ram/student/dal/repos/StudentRepository.java
+++ studentdal/src/main/java/com/ram/student/dal/repos/StudentRepository.java
@@ -2,6 +2,8 @@ package com.ram.student.dal.repos;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface StudentRepository extends CrudRepository<T, ID> {
+import com.ram.student.dal.entities.Student;
+//need two generic types the class that the repo is being made for and the type of primary key in the class
+public interface StudentRepository extends CrudRepository<Student, Long> {
 
 }
